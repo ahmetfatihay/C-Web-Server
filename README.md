2aab2dd36a046534100414226891ad30255ceaee
ahmetfatihay <77299600+ahmetfatihay@users.noreply.github.com>
2021-06-06 18:07:50 +0300

    task3b finish

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 - [x] Task 1: Create a basic TCP server (socket, bind, listen)  
 - [x] Task 2: Use multithreading (accept, pthread_create)
 - [x] Task 3_a: Get the HTTP req (recv)
-- [ ] Task 3_b: Split request headers
+- [x] Task 3_b: Split request headers
+- [ ] Task 3_c: Send HTTP status
--- server.c
+++ server.c
@@ -14,13 +14,21 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define PORT 18000
+#define PORT 18001
 #define MAXLINE 4096
 
 typedef struct sockaddr_in SA_IN;
 typedef struct sockaddr SA;
 
 void* handle_connection(void* p_client_socket);
+char* http_baslik_istegi(const char * istek);
+
+const char *HTTP_404_CONTENT = "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1>The requested resource could not be found but may be available again in the future.<div style=\"color: #eeeeee; font-size: 8pt;\">Actually, it probably won't ever be available unless this is showing up because of a bug in your program. :(</div></html>";
+const char *HTTP_501_CONTENT = "<html><head><title>501 Not Implemented</title></head><body><h1>501 Not Implemented</h1>The server either does not recognise the request method, or it lacks the ability to fulfill the request.</body></html>";
+
+const char *HTTP_200_STRING = "OK";
+const char *HTTP_404_STRING = "Not Found";
+const char *HTTP_501_STRING = "Not Implemented";
 
 int main(int argc, char **argv)
 {
@@ -87,21 +95,42 @@ void* handle_connection(void* p_client_socket)
 
     while((n = recv(client_socket, recvline, MAXLINE-1, 0)) > 0)
     {
-        printf("\nReceived:\n%s\n", recvline);
+        if(n < 0)
+        {
+            printf("Read error");
+            exit(0);
+        }
+
+        // Get request and copy
+        char recvline_copy[n];
+        strcpy(recvline_copy, recvline);
+        printf("\nReceived:\n%s\n", recvline_copy);
+
+        // Split request into headers and store it in [headers] array
+        char* headers[30];
+        int cur = 0;
+        headers[cur++] = strtok(recvline_copy, "\r\n");
+        while(headers[cur-1] != NULL)
+        {
+            headers[cur++] = strtok(NULL,"\r\n");
+        }
+
+        printf("[GET]:[%s]\n", http_baslik_istegi(headers[0]));
+
+        // Loop the headers
+        for(int i=0; headers[i] != NULL; i++)
+        {
+            printf("[%d] %s\n", i, headers[i]);
+        }
         
+        // Check if message ends and break
         if(n<4 || (recvline[n-1] == '\n' && recvline[n-2] == '\r' && recvline[n-3] == '\n' && recvline[n-4] == '\r'))
         {
             break;
         }
 
         memset(recvline, 0, MAXLINE);
-    }
-
-    if(n < 0)
-    {
-        printf("Read error");
-        exit(0);
-    }
+    }    
 
     snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\nHello");
 
@@ -110,4 +139,14 @@ void* handle_connection(void* p_client_socket)
     close(client_socket);
 
     return NULL;
+}
+
+char* http_baslik_istegi(const char* istek)
+{
+    char istek_copy[strlen(istek)];
+    strcpy(istek_copy, istek);
+    char* request_return;
+    request_return = strtok(istek_copy," ");
+    request_return = strtok(NULL," ");
+    return request_return;
 }
